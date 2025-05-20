ea00114c52cf32a5532908fcb411560abb7380d9
Xavi Alfaro <nxb.developer@gmail.com>
2025-05-20 08:37:16 -0500

    update greetings

--- src/components/greeting.tsx
+++ src/components/greeting.tsx
@@ -4,13 +4,13 @@ export function Greeting() {
   return (
     <div
       key="overview"
-      className="max-w-3xl mx-auto md:mt-20 px-8 size-full flex flex-col justify-center"
+      className="max-w-3xl mx-auto md:mt-4 px-8 size-full flex flex-col justify-center"
     >
       <motion.div
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: 10 }}
-        transition={{ delay: 0.5 }}
+        transition={{ delay: 0.3 }}
         className="text-2xl font-semibold"
       >
         Hello there!
@@ -19,10 +19,19 @@ export function Greeting() {
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: 10 }}
-        transition={{ delay: 0.6 }}
+        transition={{ delay: 0.5 }}
         className="text-zinc-500"
       >
-        How can I help you today?
+        <div className="rounded-lg text-white border border-purple-400/50 bg-purple-400/5 p-4 mt-4">
+          <p className="text-sm mb-2">I can help you with:</p>
+          <ul className="text-sm space-y-1 list-disc pl-5">
+            <li>Creating new organizations</li>
+            <li>Viewing your profile information</li>
+            <li>Showing your organizations</li>
+            <li>Managing members and roles</li>
+            <li>Sending and managing invitations</li>
+          </ul>
+        </div>
       </motion.div>
     </div>
   );
